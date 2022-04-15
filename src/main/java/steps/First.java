e88553a9f78d3ce58b4b65d417caa98f54ba1db0
Innocento <omidoinnocent32@gmail.com>
2022-04-15 19:51:29 +0300

    my java project

---
+++ src/main/java/steps/First.java
@@ -0,0 +1,7 @@
+package steps;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("I guess we all have to start from hello world even when we did it a long time ago");
+    }
+}
