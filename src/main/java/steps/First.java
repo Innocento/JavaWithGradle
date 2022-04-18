17ccfeea6f299699e257c8e82ee2939b39067ce3
Innocento <omidoinnocent32@gmail.com>
2022-04-18 22:21:13 +0300

    another java commit

--- src/main/java/steps/First.java
+++ src/main/java/steps/First.java
@@ -3,5 +3,7 @@ package steps;
 public class First {
     public static void main(String[] args) {
         System.out.println("I guess we all have to start from hello world even when we did it a long time ago");
+        System.out.println("We are doing this");
+
     }
 }
